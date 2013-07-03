2315f138db5e96343dea1d6e13330e05e5f9ebb2
L Lumbreras <llumbreras@gmail.com>
2013-07-03 12:11:39 -0700

    Create method for generating file

---
+++ lib/event_admin_v7_formletter_method.rb
@@ -0,0 +1,46 @@
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
+
+Sunlight::Congress.api_key = "API KEY"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def thankyou_letter(id,form_letter)
+	Dir.mkdir("output") unless Dir.exists? ("output")
+	filename = "output/thanks_#{id}.html"
+
+	File.open(filename,'w') do |file|
+		file.puts form_letter
+	end
+end
+
+
+puts "EventAdmin getting started."
+
+template_letter = File.read 'formletter.erb'
+erb_template = ERB.new template_letter
+
+contents = CSV.open 'event_visitors.csv', headers: true, header_converters: :symbol
+
+contents.each do | content |
+
+	id = content[0]
+	name = content[:first_name]
+
+	zipcode = clean_zipcode(content[:zipcode])
+
+	legislators = legislators_by_zipcode(zipcode)
+
+	form_letter = erb_template.result(binding)
+
+	thankyou_letter(id,form_letter)
+
+end
+
